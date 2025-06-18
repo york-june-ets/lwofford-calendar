fd214a62ee66d4690520fc8970169d87723d47cb
whyvernwhy <213481163+whyvernwhy@users.noreply.github.com>
2025-06-18 10:43:54 -0500

    Set up Welcome HTML

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,4 +1,41 @@
 
 export const WelcomePage: React.FC = () => {
-	return <h1>Welcome</h1>
+
+	function LoginOrRegister() {
+
+	}
+
+	function Login() {
+		
+	}
+
+	function Register() {
+		
+	}
+
+	return <main className="page">
+		<>
+			<h1>Welcome</h1>
+			<p>Email</p>
+			<input
+				type="email"
+			/>
+			<p>Password</p>
+			<input
+				type="password"
+			/>
+			<button>Login or register</button>
+			<p>You are creating a new user. We need some extra details to get started.</p>
+			<p>Display Name</p>
+			<input
+				type="text"
+				placeholder="Display Name..."
+			/>
+			<button>Register</button>
+		</>
+		<>
+			<h1>Welcome, Known User</h1>
+			<button>Logout</button>
+		</>
+	</main>
 }
