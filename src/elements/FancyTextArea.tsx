b3b3678775294b98da5d3cea74f3dc406aa02e28
whyvernwhy <213481163+whyvernwhy@users.noreply.github.com>
2025-06-18 15:23:01 -0500

    Added FancyTextArea from Notes project

---
+++ src/elements/FancyTextArea.tsx
@@ -0,0 +1,37 @@
+import React, { ChangeEvent } from "react";
+
+interface IFancyTextArea {
+	value: string
+	onChange: (event: ChangeEvent<HTMLTextAreaElement>) => void
+	id?: string
+	placeholder?: string
+}
+
+const FancyTextArea: React.FC<IFancyTextArea> = ({
+	value,
+	onChange,
+	id,
+	placeholder = "Write something down..."
+}) => {
+	const textAreaRef = React.useRef<HTMLTextAreaElement | null>(null)
+
+	const handleInput = (event: ChangeEvent<HTMLTextAreaElement>) => {
+		const textAreaElement = textAreaRef.current
+		if (textAreaElement) {
+			textAreaElement.style.height = "auto"
+			textAreaElement.style.height = `${textAreaElement.scrollHeight}px`
+		}
+		onChange(event)
+	}
+
+	return <textarea
+		className="auto-textarea"
+		id={id}
+		ref={textAreaRef}
+		value={value}
+		onChange={handleInput}
+		placeholder={placeholder}
+	/>
+}
+
+export default FancyTextArea
